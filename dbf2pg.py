cdc0cf258f1db8520ac5c497d95c0397cb406ebf
FoxCarlos Trabajo <foxcarlos@gmail.com>
2015-10-20 14:40:47 -0430

    prueba

--- dbf2pg.py
+++ dbf2pg.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import sys
 import dbf
 from rutinas.varias import *  # https://github.com/foxcarlos/rutinas.git
@@ -8,7 +7,7 @@ import datetime
 import psycopg2
 
 class dbf2pg():
-    '''Clase que permite exportar una tabla .dbf de foxpro o visual foxpro 
+    '''Clase que permite exportar una tabla .dbf de foxpro o visual foxpro
     tanto su estructura como los registros hacia una tabla de una base de datos
     de PostGreSQL'''
     def __init__(self):
@@ -49,11 +48,11 @@ class dbf2pg():
         self.tablaDbf.open()
 
     def crearTablaPg(self):
-        '''Metodo que permite tomar la estructura de una tabla .DBF y 
+        '''Metodo que permite tomar la estructura de una tabla .DBF y
         crear una tabla en PostgreSQL con la misma estructura '''
 
         fields = ''
-        for campo in self.tablaDbf.field_names:        
+        for campo in self.tablaDbf.field_names:
             #Prepara los campos para hacer el Create Table - Prepare the fields to make the "Create Table"
             tipo, long, long2, tipo2 = self.tablaDbf.field_info(campo)
             if tipo == 'C':
@@ -70,13 +69,13 @@ class dbf2pg():
                 c = '{0} integer ,'.format(campo)
             elif tipo == 'N':
                 c = '{0} numeric({1},{2}) ,'.format(campo, long, long2)
-            fields = fields + c     
-        
+            fields = fields + c
+
         crearTabla = 'CREATE TABLE {0} ({1})'.format(self.nombreTablaPg, fields[:-1].strip())
         return crearTabla
 
     def insertarReg(self):
-        '''Metodo que permite tomar los registros de una tabla .DBF 
+        '''Metodo que permite tomar los registros de una tabla .DBF
         e insertarlos en una tabla en postgreSQL'''
 
         separador = ','
@@ -86,7 +85,7 @@ class dbf2pg():
         listaInsert = []
         for r in self.tablaDbf:
             x = [f for f in  r]
-            valorValue = ''    
+            valorValue = ''
             for l in x:
                 #type(1L) es Long type(1) es Entero y type(1.0) es Float
                 if type(l) in [type(1L), type(1), type(1.0)]:
@@ -112,14 +111,14 @@ class dbf2pg():
         ''' '''
         self.crearTablaPG = crearTabla
         self.insertarRegPG = insertarReg
-        
+
         if self.crearTablaPG or self.insertarRegPG:
             cadenaConexion = "host='10.121.6.12' dbname='coromoto' user='admhc' password='shc21152115'"
             self.pgConectar(cadenaConexion)
 
             if self.crearTablaPG:
                 self.pgEjecutar(self.crearTablaPG)
-                
+
             if self.insertarRegPG:
                 for fila in self.insertarRegPG:
                     self.pgEjecutar(fila)
@@ -128,11 +127,11 @@ class dbf2pg():
 
 if __name__ == '__main__':
     app = dbf2pg()
-    
+
     #Indicar el nombre de la tabla .dbf que se desea exportar
     app.nombreTablaDbf = '/media/serv_coromoto/farmacia/datos/farmacos.dbf'
     app.abrirTablaDbf()
-    
+
     #Indicar el Nombre de la tabla en PostGreSQL que se desea crear
     #En Mi Caso  codigob es un esquema y farmacos es la tabla en PG
     app.nombreTablaPg = 'codigob.farmacos'
